cdeb394427a470327cb71c0e02e07a92e4a23f01
Alexander Podvoyskiy <alexander.podvoyskiy@vkteam.ru>
2026-02-03 19:16:53 +0300

    [chore]: update bundling

---
+++ src/weights_auto_tune_pipeline/constants.py
@@ -0,0 +1,24 @@
+from enum import StrEnum, auto
+
+
+class YtProxyClusterNames(StrEnum):
+    """
+    Pattern:
+      - if auto():
+          YT_PROXY_{cluster-name} -> {cluster-name}.yt.vk.team
+          Example:
+              YT_PROXY_JUPITER = auto() # jupiter.yt.vk.team
+      - else:
+          required explicit value
+          Example:
+              YT_PROXY_MIRANDA = "miranda.yt.vk.team"
+    """
+
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values) -> str:
+        _cluster_name = name.split("_")[-1].lower()
+        _POSTFIX = ".yt.vk.team"
+        return f"{_cluster_name}{_POSTFIX}"
+
+    YT_PROXY_JUPITER = auto()
+    YT_PROXY_SATURN = auto()
--- src/weights_auto_tune_pipeline/utils.py
+++ src/weights_auto_tune_pipeline/utils.py
@@ -1,18 +1,23 @@
 import typing as t
 import yt.wrapper as yt
 
+from weights_auto_tune_pipeline.constants import YtProxyClusterNames
+
 
 class YtUtils:
     @staticmethod
     def read_data_from_yt():
         pass
 
-    def _create_yt_client(
-        self,
+    @staticmethod
+    def create_yt_client(
         proxy: t.Optional[str] = None,
         token: t.Optional[str] = None,
+        config: t.Optional[dict[str, t.Any]] = None,
     ) -> yt.YtClient:
-        pass
+        return yt.YtClient(
+            proxy=proxy or YtProxyClusterNames.YT_PROXY_JUPITER, token=token, config=config
+        )
 
     @staticmethod
     def write_data_to_yt():
