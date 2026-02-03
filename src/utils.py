4d1eec868f8abc318de53fd8dfc1dad509f40453
Alexander Podvoyskiy <alexander.podvoyskiy@vkteam.ru>
2026-02-03 17:15:52 +0300

    Initial commit

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import typing as t
+import yt.wrapper as yt
+
+
+class YtUtils:
+    @staticmethod
+    def read_data_from_yt():
+        pass
+
+    def _create_yt_client(
+        self,
+        proxy: t.Optional[str] = None,
+        token: t.Optional[str] = None,
+    ) -> yt.YtClient:
+        pass
+
+    @staticmethod
+    def write_data_to_yt():
+        pass
+
+
+if __name__ == "__main__":
+    pass
