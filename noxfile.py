70c52da963d0bfcb3de42423683e615e0076cd05
Alexander Podvoyskiy <alexander.podvoyskiy@vkteam.ru>
2026-02-03 22:21:05 +0300

    [chore]: add noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,17 @@
+import nox
+
+
+PYTHON_VERSION_DEFAULT = "3.11"
+VENV_BACKEND = "uv"
+
+nox.needs_version = ">=2023.04.22"
+nox.options.default_venv_backend = VENV_BACKEND
+nox.options.verbose = True
+
+
+@nox.session(
+    python=[PYTHON_VERSION_DEFAULT], venv_backend=VENV_BACKEND, reuse_venv=True
+)
+def test(session):
+    session.install("uv", "sync")
+    session.run("pytest")
