d420fee22e9a45b2997625213f0f5f57e19e35df
Alexander Podvoyskiy <alexander.podvoyskiy@vkteam.ru>
2026-02-05 20:57:57 +0300

    [tests]: update tests

--- tests/test_gauc.py
+++ tests/test_gauc.py
@@ -4,6 +4,13 @@ from auto_tune_weights_pipeline.events import Events
 from auto_tune_weights_pipeline.target_config import TargetConfig
 from auto_tune_weights_pipeline.metrics.gauc import GAUC
 from auto_tune_weights_pipeline.columns import Columns
+from auto_tune_weights_pipeline.types_ import StrPath
+
+
+def _get_rid_from_pool_cache_file_name(path_to_pool_cache_file: StrPath) -> str:
+    return ".".join(
+        str(Path(path_to_pool_cache_file).with_suffix("")).split("__")[-1].split("_")
+    )
 
 
 class TestGaucCalculator:
@@ -22,17 +29,18 @@ class TestGaucCalculator:
         }
 
     @pytest.mark.auc
-    def test_auc_calculating(
+    def test_auc_calculate__watch_coverage_30s__4209389434_2808_1769884974477_49306(
         self,
         test_data_dir,
         target_config_watch_coverage_30s,
     ):
-        path_to_pool_cache = test_data_dir.joinpath(
-            "./AUC=1_0/pool_cache_2026_01_31_rid=4209389434_2808_1769884974477_49306.jsonl"
-        )
         target_event = "watch_coverage_30s"
+        path_to_pool_cache_file = test_data_dir.joinpath(
+            "./AUC=0_0/pool_cache_2026_01_31__4209389434_2808_1769884974477_49306.jsonl"
+        )
+        rid = _get_rid_from_pool_cache_file_name(path_to_pool_cache_file)
 
-        gauc_metric = GAUC(path_to_pool_cache=path_to_pool_cache)
+        gauc_metric = GAUC(path_to_pool_cache=path_to_pool_cache_file)
         results = gauc_metric.calculate_metric(
             target_configs=target_config_watch_coverage_30s,
             session_col_name=Columns.RID_COL_NAME,
@@ -45,26 +53,75 @@ class TestGaucCalculator:
             "AUC": 0.0,
             "AUC_error": None,
             "AUC_valid": True,
-            "GAUCSimple": 1.0,
-            "GAUCWeighted": 1.0,
+            "GAUCSimple": 0.0,
+            "GAUCWeighted": 0.0,
             "GAUC_valid": True,
             "group_details": [
                 {
-                    "auc": 1.0,
+                    "auc": 0.0,
                     "negatives": 1,
                     "positive_rate": 0.5,
                     "positives": 1,
-                    "session_id": "4209389434.2808.1769884974477.49306",
+                    "session_id": rid,
                     "size": 2,
                 },
             ],
-            "max": 1.0,
-            "median": 1.0,
-            "min": 1.0,
+            "max": 0.0,
+            "median": 0.0,
+            "min": 0.0,
             "n_groups": 1,
             "positive_rate": 0.5,
             "std": 0.0,
-            "target": "watch_coverage_30s",
+            "target": target_event,
             "total_positives": 1,
             "total_samples": 2,
+        }
+
+    @pytest.mark.auc
+    def test_auc_calculate__watch_coverage_30s__918960451_4009_1769879722682_60050(
+        self,
+        test_data_dir,
+        target_config_watch_coverage_30s,
+    ):
+        target_event = "watch_coverage_30s"
+        path_to_pool_cache_file = test_data_dir.joinpath(
+            f"./AUC=0_0/pool_cache_2026_01_31__918960451_4009_1769879722682_60050.jsonl"
+        )
+        rid = _get_rid_from_pool_cache_file_name(path_to_pool_cache_file)
+
+        gauc_metric = GAUC(path_to_pool_cache=path_to_pool_cache_file)
+        results = gauc_metric.calculate_metric(
+            target_configs=target_config_watch_coverage_30s,
+            session_col_name=Columns.RID_COL_NAME,
+            nav_screen="video_for_you",
+            platform="vk_video_android",
+            calculate_regular_auc=True,
+        )
+
+        assert results[target_event] == {
+            "AUC": 0.0,
+            "AUC_error": None,
+            "AUC_valid": True,
+            "GAUCSimple": 0.0,
+            "GAUCWeighted": 0.0,
+            "GAUC_valid": True,
+            "group_details": [
+                {
+                    "auc": 0.0,
+                    "negatives": 2,
+                    "positive_rate": 0.3333333333333333,
+                    "positives": 1,
+                    "session_id": rid,
+                    "size": 3,
+                },
+            ],
+            "max": 0.0,
+            "median": 0.0,
+            "min": 0.0,
+            "n_groups": 1,
+            "positive_rate": 0.3333333333333333,
+            "std": 0.0,
+            "target": target_event,
+            "total_positives": 1,
+            "total_samples": 3,
         }
