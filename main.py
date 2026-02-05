07c72521915977655b6eda99f6f314c400f451c2
Alexander Podvoyskiy <alexander.podvoyskiy@vkteam.ru>
2026-02-05 11:10:05 +0300

    [chore]: update bundling

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import typing as t
 
+from loguru import logger
 from auto_tune_weights_pipeline.columns import Columns
 from auto_tune_weights_pipeline.metrics.gauc import GAUC
 from auto_tune_weights_pipeline.target_config import TargetConfig
 from auto_tune_weights_pipeline.events import Events
+from auto_tune_weights_pipeline.logging_ import setup_logging
+
+setup_logging()
 
 
 # TODO: add @click
@@ -16,16 +20,18 @@ def main():
         )
     }
 
-    result = GAUC(
-        path_to_pool_cache="./data/pool_cache_2026_01_31.jsonl"
-    ).calculate_metric(
+    gauc_metric = GAUC(path_to_pool_cache="./data/pool_cache_2026_01_31.jsonl")
+    results = gauc_metric.calculate_metric(
         target_configs=target_config,
         session_col_name=Columns.RID_COL_NAME,
         nav_screen="video_for_you",
         platform="vk_video_android",
+        calculate_regular_auc=True,
     )
+    logger.info(results)
 
-    print(result)
+    summary = gauc_metric.get_summary(results)
+    print(summary)
 
 
 if __name__ == "__main__":
