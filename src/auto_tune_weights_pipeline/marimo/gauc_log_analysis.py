d0ce4ba38948f8737899d69965e20a3d935f76f2
Alexander Podvoyskiy <alexander.podvoyskiy@vkteam.ru>
2026-02-06 04:16:44 +0300

    [chore]: update figures

--- src/auto_tune_weights_pipeline/marimo/gauc_log_analysis.py
+++ src/auto_tune_weights_pipeline/marimo/gauc_log_analysis.py
@@ -6,13 +6,22 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    import marimo as mo
     import numpy as np
     import pandas as pd
     import matplotlib.pyplot as plt
 
     from pathlib import Path
     from auto_tune_weights_pipeline.utils import LogParser
-    return LogParser, Path, np, pd, plt
+    return LogParser, Path, mo, np, pd, plt
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### _AUX functions_
+    """)
+    return
 
 
 @app.cell
@@ -37,10 +46,10 @@ def _(Path, np, pd, plt):
 
         if save_fig:
             path_to_figures_dir = Path.cwd() / figures_dir
-        
+
             if not path_to_figures_dir.exists(): 
                 path_to_figures_dir.mkdir(exist_ok=True)
-            
+
             fig.savefig(f"{path_to_figures_dir / title}.png")
 
         return fig
@@ -59,50 +68,58 @@ def _(Path, pd, plt):
         figures_dir: str = "figures",
     ) -> None:
         fig, ax = plt.subplots(figsize=(10, 5))
-    
+
         aucs.plot.kde(ax=ax)
 
         ax.text(text_x, text_y, text)
-    
+
         ax.set_title(title)
         ax.set_xlim(0.0, 1.0)
         ax.set_xlabel("AUC")
-    
+
         if save_fig:
             path_to_figures_dir = Path.cwd() / figures_dir
-        
+
             if not path_to_figures_dir.exists(): 
                 path_to_figures_dir.mkdir(exist_ok=True)
-            
+
             fig.savefig(f"{path_to_figures_dir / title}.png")
 
         return fig
     return (plot_auc_kde_summary,)
 
 
-@app.cell
-def _(LogParser, np):
-    aucs_2026_02_01: np.ndarray = LogParser.read_logs_to_aucs("./logs/logs_for_pool_cache_2026_02_01.log")
-    return (aucs_2026_02_01,)
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### _watch_coverage_30s_
+    """)
+    return
 
 
 @app.cell
 def _(LogParser, np):
-    aucs_2026_02_02: np.ndarray = LogParser.read_logs_to_aucs("./logs/logs_for_pool_cache_2026_02_02.log")
-    return (aucs_2026_02_02,)
+    aucs_2026_02_01_watch_coverage_30s: np.ndarray = LogParser.read_logs_to_aucs("./logs/logs_for_pool_cache_2026_02_01.log", target_name="watch_coverage_30s")
+    return (aucs_2026_02_01_watch_coverage_30s,)
 
 
 @app.cell
 def _(LogParser, np):
-    aucs_2026_02_03: np.ndarray = LogParser.read_logs_to_aucs("./logs/logs_for_pool_cache_2026_02_03.log")
-    return (aucs_2026_02_03,)
+    aucs_2026_02_02_watch_coverage_30s: np.ndarray = LogParser.read_logs_to_aucs("./logs/logs_for_pool_cache_2026_02_02.log", target_name="watch_coverage_30s")
+    return (aucs_2026_02_02_watch_coverage_30s,)
 
 
 @app.cell
-def _(aucs_2026_02_01: "np.ndarray", plot_auc_kde):
+def _(LogParser, np):
+    aucs_2026_02_03_watch_coverage_30s: np.ndarray = LogParser.read_logs_to_aucs("./logs/logs_for_pool_cache_2026_02_03.log", target_name="watch_coverage_30s")
+    return (aucs_2026_02_03_watch_coverage_30s,)
+
+
+@app.cell
+def _(aucs_2026_02_01_watch_coverage_30s: "np.ndarray", plot_auc_kde):
     plot_auc_kde(
-        aucs_2026_02_01,
-        title="pool-cache: 2026-02-01",
+        aucs_2026_02_01_watch_coverage_30s,
+        title="pool-cache: 2026-02-01 (watch_coverage_30s)",
         text="AUC: 0.7105\nAUC (weighted): 0.6115\nGAUC (simple): 0.6375",
         text_x=0.05,
         text_y=5,
@@ -112,10 +129,10 @@ def _(aucs_2026_02_01: "np.ndarray", plot_auc_kde):
 
 
 @app.cell
-def _(aucs_2026_02_02: "np.ndarray", plot_auc_kde):
+def _(aucs_2026_02_02_watch_coverage_30s: "np.ndarray", plot_auc_kde):
     plot_auc_kde(
-        aucs_2026_02_02,
-        title="pool-cache: 2026-02-02",
+        aucs_2026_02_02_watch_coverage_30s,
+        title="pool-cache: 2026-02-02 (watch_coverage_30s)",
         text="AUC: 0.7376\nGAUC (weighted): 0.6418\nGAUC (simple): 0.6963",
         text_x=0.05,
         text_y=6,
@@ -125,10 +142,10 @@ def _(aucs_2026_02_02: "np.ndarray", plot_auc_kde):
 
 
 @app.cell
-def _(aucs_2026_02_03: "np.ndarray", plot_auc_kde):
+def _(aucs_2026_02_03_watch_coverage_30s: "np.ndarray", plot_auc_kde):
     plot_auc_kde(
-        aucs_2026_02_03,
-        title="pool-cache: 2026-02-03",
+        aucs_2026_02_03_watch_coverage_30s,
+        title="pool-cache: 2026-02-03 (watch_coverage_30s)",
         text="AUC: 0.7339\nAUC (weighted): 0.6368\nGAUC (simple): 0.6918",
         text_x=0.05,
         text_y=6,
@@ -139,21 +156,21 @@ def _(aucs_2026_02_03: "np.ndarray", plot_auc_kde):
 
 @app.cell
 def _(
-    aucs_2026_02_01: "np.ndarray",
-    aucs_2026_02_02: "np.ndarray",
-    aucs_2026_02_03: "np.ndarray",
+    aucs_2026_02_01_watch_coverage_30s: "np.ndarray",
+    aucs_2026_02_02_watch_coverage_30s: "np.ndarray",
+    aucs_2026_02_03_watch_coverage_30s: "np.ndarray",
     pd,
     plot_auc_kde_summary,
 ):
-    _min = min(aucs_2026_02_01.size, aucs_2026_02_02.size)
+    _min = min(aucs_2026_02_01_watch_coverage_30s.size, aucs_2026_02_02_watch_coverage_30s.size)
 
     plot_auc_kde_summary(
         pd.DataFrame({
-            "2026-02-01": aucs_2026_02_01[:_min],
-            "2026-02-02": aucs_2026_02_02[:_min],
-            "2026-02-03": aucs_2026_02_03[:_min],
+            "2026-02-01": aucs_2026_02_01_watch_coverage_30s[:_min],
+            "2026-02-02": aucs_2026_02_02_watch_coverage_30s[:_min],
+            "2026-02-03": aucs_2026_02_03_watch_coverage_30s[:_min],
         }),
-        title="summary",
+        title="summary (watch_coverage_30s)",
         text=(
             "navScreen: video_for_you,\n"
             "platform: vk_video_android,\n"
