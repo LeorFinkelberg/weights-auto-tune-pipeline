7e04b74b289e9f3c8d59ff895ac37f068b67f92a
Alexander Podvoyskiy <alexander.podvoyskiy@vkteam.ru>
2026-02-10 14:17:25 +0300

    [chore]: update run-script

--- run_tune_pipeline.sh
+++ run_tune_pipeline.sh
@@ -2,7 +2,6 @@ uv run cli.py \
     --path-to-pool-cache-train ./data/pool_cache_with_features_2026_02_01_train.jsonl \
     --path-to-pool-cache-val ./data/pool_cache_with_features_2026_02_02_val.jsonl \
     --loss-function PairLogitPairwise \
-    --iterations 300 \
-    --n-trials 3 \
-    --timeout 600 \
+    --timeout 3600 \
+    --n-trials 50 \
     --no-load-if-exists
